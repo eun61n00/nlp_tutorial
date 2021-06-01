198bcd8251ca31e702ce391ca8c784d708e90a47
Sunglok Choi <sunglok@hanmail.net>
2021-06-01 12:46:17 +0900

    Add examples of word2vec (fastText) using PyTorch

---
+++ nlp02_word2vec_pytorch.py
@@ -0,0 +1,12 @@
+import torch, torchtext
+from sklearn.metrics.pairwise import cosine_similarity
+
+word2vec = torchtext.vocab.FastText('en') # It downloads a lookup table for word embedding.
+words = ['dog', 'cat', 'apple', 'banana', 'asdfg'] # Try your examples
+word_vectors =[word2vec[word].numpy() for word in words]
+
+# Print the default similarity of several pairs
+print('### Consine similarity')
+for (i, j) in [(0, 1), (0, 2), (0, 4)]:
+    cosine = cosine_similarity([word_vectors[i]], [word_vectors[j]])[0,0]
+    print(f'* {words[i]} - {words[j]}: {cosine:.3f}')
